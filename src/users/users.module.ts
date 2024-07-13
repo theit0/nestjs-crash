a814649fab1ad03a0062937115366805824a234d
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-07-13 13:20:24 -0300

    example middleware for an especific route (like '/users')

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
+import { ExampleMiddlewareMiddleware } from './middlewares/example_middleware/example_middleware.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ExampleMiddlewareMiddleware).forRoutes('users')
+  }
+}
