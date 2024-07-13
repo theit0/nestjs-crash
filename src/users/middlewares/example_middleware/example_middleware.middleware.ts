b030b8125938d88933b9766af6462f33c1206de3
Theo Pelegrina <71673834+theit0@users.noreply.github.com>
2024-07-13 14:40:25 -0300

    middleware authorization example, getting de header and validating it 'example_middleware.middleware.ts'

--- src/users/middlewares/example_middleware/example_middleware.middleware.ts
+++ src/users/middlewares/example_middleware/example_middleware.middleware.ts
@@ -1,9 +1,15 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class ExampleMiddlewareMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     console.log("Hi! Im example middleware")
-    next();
+    console.log(req.headers.authorization)
+    const { authorization } = req.headers;
+    if (!authorization) throw new HttpException('No authorization token', HttpStatus.FORBIDDEN)
+    if (authorization === "exampletoken") next();
+    else
+      throw new HttpException('Invalid authorization token', HttpStatus.FORBIDDEN)
   }
 }
